4bd635bfec7cc49d0a1b5df88039fcc04bc14e6b
vedpra55 <vedna400@gmail.com>
2021-12-26 16:08:03 +0530

    add url

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,13 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host : env('DATABASE_HOST'),
+        host : env('DATABASE_HOST', 'fiverblog.s414a.mongodb.net'),
         srv: env('DATABASE_SRV', true),
         port: env('DATABASE_PORT', 27017),
-        database : env('DATABASE_NAME'),
-        username: env('DATABASE_USER'),
-        password: env('DATABASE_PASSWORD'),
-        uri : env("DATABASE_URI")
+        database : env('DATABASE_NAME', 'fiverBlog4'),
+        username: env('DATABASE_USER', 'vedpratapsingh'),
+        password: env('DATABASE_PASSWORD', 'narayanji100'),
+        uri : env("DATABASE_URI", 'mongodb://vedpratapsingh:narayanji100@fiverblog4-shard-00-00.s414a.mongodb.net:27017,fiverblog-shard-00-01.s414a.mongodb.net:27017,fiverblog-shard-00-02.s414a.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-qbidh7-shard-0&authSource=admin&retryWrites=true&w=majority')
       },
       options: {
         authenticationDatabase: env('AUTH_DATABASE', null),
